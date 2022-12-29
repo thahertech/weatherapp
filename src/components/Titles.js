265593de52a35b2da44faa34804b16e7506fe3f2
Thaher Al Amir <thaher.alamir@hotmail.com>
2022-12-29 19:48:13 +0200

    First Draft Updates

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather APP</h1>
+		<h2 className="title-container__subtitle">All weather details from here</h2>
+	</div>
+);
+
+export default Titles;
